4670d35c0e086b1b00ec5777bcb10220ce5e6add
Grzegorz Wilczynski <grzegorz.wilczynski@infopark.de>
2021-05-07 15:29:58 +0200

    If this header is present, then the request is treated as a "background request"
    read more:
    https://www.scrivito.com/js-sdk/configur

--- scrivito_import_export/rest_api.rb
+++ scrivito_import_export/rest_api.rb
@@ -1,10 +1,10 @@
-require "addressable/uri"
-require "net/http"
-require "net/http/post/multipart"
-require "openssl"
+require 'addressable/uri'
+require 'net/http'
+require 'net/http/post/multipart'
+require 'openssl'
 
 class RestApi
-  class ScrivitoError < StandardError ; end
+  class ScrivitoError < StandardError; end
 
   def initialize(base_url, tenant, api_key)
     @base_url = base_url
@@ -29,9 +29,9 @@ class RestApi
   end
 
   def upload_future_binary(file_to_upload, filename, obj_id, content_type: nil)
-    permission = get("blobs/upload_permission")
+    permission = get('blobs/upload_permission')
     upload = upload_file(file_to_upload, nil, permission)
-    put("blobs/activate_upload",
+    put('blobs/activate_upload',
         filename: filename, content_type: content_type, upload: upload, obj_id: obj_id)
   end
 
@@ -43,16 +43,16 @@ class RestApi
 
   def request_cms_api(method, path, payload)
     response = response_for_request_cms_api(method, path, payload)
-    if response.is_a?(Hash) && response.keys == ["task"] && response["task"].is_a?(Hash)
-      task_path = "tasks/#{response["task"]["id"]}"
+    if response.is_a?(Hash) && response.keys == ['task'] && response['task'].is_a?(Hash)
+      task_path = "tasks/#{response['task']['id']}"
       begin
-        sleep(2 + rand())
+        sleep(2 + rand)
         task_data = response_for_request_cms_api(Net::HTTP::Get, task_path, nil)
-      end until task_data["status"] != "open"
-      if task_data["status"] == "success"
-        task_data["result"]
+      end until task_data['status'] != 'open'
+      if task_data['status'] == 'success'
+        task_data['result']
       else
-        raise ScrivitoError.new("412 #{task_data["code"]} #{task_data["message"]}")
+        raise ScrivitoError, "412 #{task_data['code']} #{task_data['message']}"
       end
     else
       response
@@ -61,17 +61,18 @@ class RestApi
 
   def response_for_request_cms_api(method, path, payload)
     req = method.new(URI.parse("#{@base_url}/tenants/#{@tenant}/#{path}"))
-    req.basic_auth("api_token", @api_key)
-    req["Content-type"] = "application/json"
-    req["Accept"] = "application/json"
+    req.basic_auth('api_token', @api_key)
+    req['Content-type'] = 'application/json'
+    req['Accept'] = 'application/json'
+    req['Scrivito-Priority'] = 'background'
     req.body = JSON.dump(payload) if payload.present?
     response = retry_on_rate_limit(Time.now + 25.seconds) do
       perform_request(req)
     end
-    if response.code.start_with?("2")
+    if response.code.start_with?('2')
       JSON.load(response.body)
     else
-      raise ScrivitoError.new("#{response.code} #{response.body}")
+      raise ScrivitoError, "#{response.code} #{response.body}"
     end
   end
 
@@ -81,13 +82,13 @@ class RestApi
 
   def internal_retry(request_proc, timeout, backoff_wait_time)
     response = request_proc.call
-    if response.code == "429"
+    if response.code == '429'
       if Time.now < timeout
-        sleep_time = [backoff_wait_time, response["Retry-After"].to_f].max
+        sleep_time = [backoff_wait_time, response['Retry-After'].to_f].max
         sleep(sleep_time)
         internal_retry(request_proc, timeout, 2 * backoff_wait_time)
       else
-        raise ScrivitoError.new("429 rate limit exceeded")
+        raise ScrivitoError, '429 rate limit exceeded'
       end
     else
       response
@@ -112,13 +113,13 @@ class RestApi
   def upload_file(file, content_type, upload_permission)
     File.open(file) do |open_file|
       upload_io = UploadIO.new(open_file, content_type, File.basename(file))
-      params = upload_permission["fields"].merge("file" => upload_io)
-      uri = URI.parse(upload_permission["url"])
+      params = upload_permission['fields'].merge('file' => upload_io)
+      uri = URI.parse(upload_permission['url'])
       uri.normalize!
       req = Net::HTTP::Post::Multipart.new(uri, params)
       response = perform_request(req)
-      if response.code.starts_with?("2")
-        upload_permission["blob"]
+      if response.code.starts_with?('2')
+        upload_permission['blob']
       else
         raise ScrivitoError, "File upload failed with code #{response.code}"
       end
